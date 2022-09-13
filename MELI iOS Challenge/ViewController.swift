b7b3da9a4bcd5565b95afb49166668797605fe9f
fcamilletti <franco.camilletti@mercadolibre.com>
2022-09-13 08:22:51 -0300

    Project first steps

---
+++ MELI iOS Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MELI iOS Challenge
+//
+//  Created by Franco Camilletti on 13/09/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
---
+++ MELI iOS ChallengeUITests/MELI_iOS_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MELI_iOS_ChallengeUITestsLaunchTests.swift
+//  MELI iOS ChallengeUITests
+//
+//  Created by Franco Camilletti on 13/09/2022.
+//
+
+import XCTest
+
+class MELI_iOS_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
