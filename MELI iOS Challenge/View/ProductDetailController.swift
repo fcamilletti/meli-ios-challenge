e42cdd063a27160b9e6407dc4373b2b9d8d152a6
fcamilletti <franco.camilletti@mercadolibre.com>
2022-09-13 14:39:28 -0300

    First components, files and rearrangement

---
+++ MELI iOS Challenge/View/ProductDetailController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductDetailController.swift
+//  MELI iOS Challenge
+//
+//  Created by Franco Camilletti on 13/09/2022.
+//
+
+import UIKit
+
+class ProductDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
